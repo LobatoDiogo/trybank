d81595da3b5a96ee55b79f73e0412316d555a406
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-07-05 16:19:22 -0300

    register account done

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -4,7 +4,7 @@ public class Trybank
 {
     public bool Logged;
     public int loggedUser;
-    
+
     //0 -> Número da conta
     //1 -> Agência
     //2 -> Senha
@@ -23,8 +23,28 @@ public class Trybank
     // 1. Construa a funcionalidade de cadastrar novas contas
     public void RegisterAccount(int number, int agency, int pass)
     {
-        //inicio
-        throw new NotImplementedException();
+        try
+        {
+            for (int i = 0; i < maxAccounts; i++)
+            {
+                if (Bank[i, 0] == number && Bank[i, 1] == agency)
+                {
+                    throw new ArgumentException("A conta já está sendo usada!");
+                }
+            }
+            
+            Bank[registeredAccounts, 0] = number;
+            Bank[registeredAccounts, 1] = agency;
+            Bank[registeredAccounts, 2] = pass;
+            Bank[registeredAccounts, 3] = 0;
+
+            registeredAccounts++;
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
     }
 
     // 2. Construa a funcionalidade de fazer Login
@@ -42,7 +62,7 @@ public class Trybank
     // 4. Construa a funcionalidade de checar o saldo
     public int CheckBalance()
     {
-        throw new NotImplementedException();   
+        throw new NotImplementedException();
     }
 
     // 5. Construa a funcionalidade de depositar dinheiro
@@ -63,5 +83,5 @@ public class Trybank
         throw new NotImplementedException();
     }
 
-   
+
 }
